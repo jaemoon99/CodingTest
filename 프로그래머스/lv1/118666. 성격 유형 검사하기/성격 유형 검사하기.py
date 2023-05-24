eead4e0356f9bdf9f7596508d3f5e4937f4e46d2
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-05-24 12:48:17 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,58 +1,34 @@
 def solution(survey, choices):
     answer = ""
-    cha = ["R", "C", "J", "A", "T", "F", "M", "N"]
-    point = [0] * len(cha)
-    for l_index in range(0, len(survey)):
-        if choices[l_index] == 1:
-            for l_cha in cha:
-                if l_cha == survey[l_index][0]:
-                    point[cha.index(survey[l_index][0])] += 3
-        elif choices[l_index] == 2:
-            for l_cha in cha:
-                if l_cha == survey[l_index][0]:
-                    point[cha.index(survey[l_index][0])] += 2
-        elif choices[l_index] == 3:
-            for l_cha in cha:
-                if l_cha == survey[l_index][0]:
-                    point[cha.index(survey[l_index][0])] += 1
-        elif choices[l_index] == 5:
-            for l_cha in cha:
-                if l_cha == survey[l_index][1]:
-                    point[cha.index(survey[l_index][1])] += 1
-        elif choices[l_index] == 6:
-            for l_cha in cha:
-                if l_cha == survey[l_index][1]:
-                    point[cha.index(survey[l_index][1])] += 2
-        elif choices[l_index] == 7:
-            for l_cha in cha:
-                if l_cha == survey[l_index][1]:
-                    point[cha.index(survey[l_index][1])] += 3
+    mbti_dic = {
+        'R' : 0,
+        'C' : 0,
+        'J' : 0,
+        'A' : 0,
+        'T' : 0,
+        'F' : 0,
+        'M' : 0,
+        'N' : 0
+    }
 
-    if point[cha.index("R")] > point[cha.index("T")]:
-        answer += 'R'
-    elif point[cha.index("R")] < point[cha.index("T")]:
-        answer += 'T'
-    elif point[cha.index("R")] == point[cha.index("T")]:
-        answer += 'R'
+    mbti_set = (('R', 'T'), ('C', 'F'), ('J', 'M'), ('A', 'N'))
 
-    if point[cha.index("C")] > point[cha.index("F")]:
-        answer += 'C'
-    elif point[cha.index("C")] < point[cha.index("F")]:
-        answer += 'F'
-    elif point[cha.index("C")] == point[cha.index("F")]:
-        answer += 'C'
+    for x in range(len(choices)):
+        if choices[x] >= 5:
+            mbti_dic[survey[x][1]] += choices[x] - 4
+        elif choices[x] == 1:
+            mbti_dic[survey[x][0]] += 3
+        elif choices[x] == 3:
+            mbti_dic[survey[x][0]] += 1
+        elif choices[x] == 2:
+            mbti_dic[survey[x][0]] += 2
 
-    if point[cha.index("J")] > point[cha.index("M")]:
-        answer += 'J'
-    elif point[cha.index("J")] < point[cha.index("M")]:
-        answer += 'M'
-    elif point[cha.index("J")] == point[cha.index("M")]:
-        answer += 'J'
+    for y in mbti_set:
+        if mbti_dic[y[0]] == mbti_dic[y[1]]:
+            answer += sorted(y)[0]
+        elif mbti_dic[y[0]] > mbti_dic[y[1]]:
+            answer += y[0]
+        elif mbti_dic[y[0]] < mbti_dic[y[1]]:
+            answer += y[1]
 
-    if point[cha.index("A")] > point[cha.index("N")]:
-        answer += 'A'
-    elif point[cha.index("A")] < point[cha.index("N")]:
-        answer += 'N'
-    elif point[cha.index("A")] == point[cha.index("N")]:
-        answer += 'A'
     return answer
