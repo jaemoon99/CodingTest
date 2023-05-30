3bacc939722794b40a8bec812d3d6387a32a5f7b
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-05-30 16:49:37 +0900

    [level 1] Title: 폰켓몬, Time: 0.20 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -3,11 +3,11 @@ def solution(nums):
     nums_set = set(nums)
     
     # 중복 제거한 길이가 절반보다 같거나 크면
-    if len(nums_set) >= len(nums) // 2:
+    if len(nums_set) < len(nums) // 2:
         
         # NUM 길이의 절반을 반환
-        return len(nums) // 2
+        return len(nums_set)
     else:
         
         # 중복제거 길이만큼 반환
-        return len(nums_set)
+        return len(nums) // 2
