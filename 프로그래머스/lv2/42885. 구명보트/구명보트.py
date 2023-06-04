5dcf0c5ddd024cf87beb44a2c5e9dc624b4e6fb7
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-06-04 17:36:15 +0900

    [level 2] Title: 구명보트, Time: 11.00 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def solution(people, limit):
+    answer = 0
+    sorted_people = deque(sorted(people, reverse=True))
+    while sorted_people:
+        max_kg = sorted_people.popleft()
+        if len(sorted_people) == 0:
+            answer += 1
+            break
+        if limit >= max_kg + sorted_people[-1]:
+            sorted_people.pop()
+        answer += 1
+    return answer
