d9829155809077e8809c0a3c517df36e3b395010
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-06-29 11:06:51 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,10 @@
+def solution(binomial):
+    binomial = binomial.split(' ')
+    if binomial[1] == '+':
+        return int(binomial[0]) + int(binomial[2])
+    elif binomial[1] == '-':
+        return int(binomial[0]) - int(binomial[2])
+    elif binomial[1] == '*':
+        return int(binomial[0]) * int(binomial[2])
+    else:
+        return int(binomial[0]) / int(binomial[2])
