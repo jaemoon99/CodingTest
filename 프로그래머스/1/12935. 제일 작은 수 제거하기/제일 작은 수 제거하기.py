f3e1da992c0a9398cc274b3fd97094cdee9bfd19
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-10-31 23:40:09 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,7 +1,6 @@
 def solution(arr):
-    min_num = sorted(arr, reverse = 1)[-1]
     if len(arr) == 1:
         return [-1]
     else:
-        arr.remove(min_num)
+        arr.remove(min(arr))
         return arr
