51e6a85cd095a32d0bb284beb4e51d454367c1f1
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-10-31 19:59:22 +0900

    [level unrated] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,7 +1,6 @@
 def solution(s):
     answer = 0
     split_list = list(s.split(' '))
-    print(split_list)
     for x in range(len(split_list)):
         if split_list[x] == "Z":
             answer -= int(split_list[x - 1])
