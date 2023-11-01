7d0fbe7f83758173c9be49cf5dafeb8fab14d1e3
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-01 23:35:24 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,2 +1,4 @@
 def solution(numbers):
-    return int(numbers.replace("one", "1").replace("two", "2").replace("three", "3").replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7").replace("eight", "8").replace("nine", "9").replace("zero", "0"))
+    for num, eng in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(eng, str(num))
+    return int(numbers)
