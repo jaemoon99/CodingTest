557c8eedec2f60c3907897084c56780b963d1f1b
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-03 01:04:41 +0900

    [level 1] Title: 옹알이 (2), Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,15 @@
+def solution(babbling):
+    answer = 0
+    note = ["aya", "ye", "woo", "ma"]
+    for i, x in enumerate(babbling):
+        for y in note:
+            if 2*y in babbling[i]:
+                babbling[i] = "-----"
+    print(babbling)
+    for i, x in enumerate(babbling):
+        for y in note:
+            if y in x:
+                babbling[i] = babbling[i].replace(y, ".")
+    for i, x in enumerate(babbling):
+        babbling[i] = babbling[i].replace(".", "")
+    return babbling.count("")
