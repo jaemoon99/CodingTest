c25e616191e7303f5f9915284af13a87ce9d816b
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-05 00:35:19 +0900

    [level unrated] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.py
@@ -1,18 +1,4 @@
 def solution(score):
-    answer = []
-    rank = 1
-    score_dict = {}
-    score = [sum(x) / 2.0 for x in score]
+    score = [sum(x) for x in score]
     sort_score = sorted(score, reverse = 1)
-    for i, x in enumerate(sort_score):
-        if i == 0:
-            score_dict[x] = rank
-            rank += 1
-        elif sort_score[i] == sort_score[i - 1]:
-            score_dict[x] = score_dict[sort_score[i - 1]]
-            rank += 1
-        else:
-            score_dict[x] = rank
-            rank += 1
-
-    return [score_dict[x] for x in score]
+    return [sort_score.index(x) + 1 for x in score]
