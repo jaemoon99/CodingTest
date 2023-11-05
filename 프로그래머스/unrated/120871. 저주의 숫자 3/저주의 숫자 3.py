bbd22fa25944e2518e55e5fbca2299f62a0e3783
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-05 20:48:03 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 1
+    count = 0
+    while 1:
+        if answer % 3 == 0 or "3" in str(answer):
+            pass
+        else:
+            count += 1
+        answer += 1
+        if count == n:
+            return answer - 1
