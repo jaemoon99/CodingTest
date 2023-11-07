c4d08464c77565c1c9e3b2a63a4da0c4706dbf7f
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-07 23:18:11 +0900

    [level unrated] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,24 @@
+def solution(polynomial):
+    answer = ''
+    terms = polynomial.split(" + ")
+    x_term, int_term = [], []
+    for term in terms:
+        if term == "x":
+            x_term.append(1)
+        elif "x" in term:
+            x_term.append(int(term.replace("x", "")))
+        else:
+            int_term.append(int(term))
+
+    if x_term:
+        if sum(x_term) == 1:
+            if int_term:
+                return f'x + {sum(int_term)}'
+            else:
+                return f'x'
+        elif int_term:
+            return f'{sum(x_term)}x + {sum(int_term)}'
+        else:
+            return f'{sum(x_term)}x'
+    else:
+        return f'{sum(int_term)}'
