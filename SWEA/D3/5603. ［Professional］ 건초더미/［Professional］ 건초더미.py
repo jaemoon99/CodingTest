5b18476cff679ddc08f3f1fc0185993ef4edcf63
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-14 19:07:48 +0900

    [D3] Title: [Professional] 건초더미, Time: 226 ms, Memory: 63,324 KB -BaekjoonHub

--- SWEA/D3/5603. ［Professional］ 건초더미/［Professional］ 건초더미.py
+++ SWEA/D3/5603. ［Professional］ 건초더미/［Professional］ 건초더미.py
@@ -2,7 +2,7 @@ T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
     S_list = sorted([int(input()) for _ in range(N)])
-    avg = sum(S_list) // len(S_list)
+    avg = int(sum(S_list) / len(S_list))
 
     count = 0
     for x in S_list:
