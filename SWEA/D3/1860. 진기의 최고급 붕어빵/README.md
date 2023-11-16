3b899bcd0f66d43647d441ef71645a8686a0fc33
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-16 22:55:03 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 470 ms, Memory: 65,716 KB -BaekjoonHub

---
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/README.md
@@ -0,0 +1,15 @@
+# [D3] 진기의 최고급 붕어빵 - 1860 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LsaaqDzYDFAXc) 
+
+### 성능 요약
+
+메모리: 65,716 KB, 시간: 470 ms, 코드길이: 349 Bytes
+
+### 제출 일자
+
+2023-11-16 22:54
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -0,0 +1,13 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    N, M, K = map(int, input().split())
+    stop = sorted(list(map(int, input().split())))
+    result = "Possible"
+
+    for x in range(N):
+        count = (stop[x] // M) * K - (x + 1)
+        if 0 > count:
+            result = "Impossible"
+            break
+    
+    print(f'#{test_case} {result}')
