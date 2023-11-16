e39e209bb3fe263010136044141ba9ef9a9d88fd
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-16 22:57:26 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 402 ms, Memory: 65,700 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -5,9 +5,9 @@ for test_case in range(1, T + 1):
     result = "Possible"
 
     for x in range(N):
-        count = (stop[x] // M) * K - (x + 1)
-        if 0 > count:
+        count = (stop[x] // M) * K - x
+        if 0 >= count:
             result = "Impossible"
             break
-    
+
     print(f'#{test_case} {result}')
