01802b49e43d4b183a7b0eb8ea06cccfef85f035
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-19 00:30:21 +0900

    [level unrated] Title: 평행, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120875. 평행/평행.py
+++ 프로그래머스/unrated/120875. 평행/평행.py
@@ -1,3 +1,7 @@
+def slope(dot1,dot2):
+    return (dot2[1] - dot1[1] ) / (dot2[0] - dot1[0])
+
+
 def solution(dots):
     answer = 0
     if slope(dots[0],dots[1]) == slope(dots[2],dots[3]):
@@ -7,6 +11,3 @@ def solution(dots):
     if slope(dots[0],dots[3]) == slope(dots[1],dots[2]):
         answer = 1
     return answer
-
-def slope(dot1,dot2):
-    return (dot2[1] - dot1[1] ) / (dot2[0] - dot1[0])
