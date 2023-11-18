477d58d65f82138ee6cca50fdd75aa9922e83d3c
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-19 00:09:14 +0900

    [level unrated] Title: 겹치는 선분의 길이, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -0,0 +1,15 @@
+def solution(lines):
+    answer = 0
+    dot_dic = dict()
+    for i, j in lines:
+        for k in range(i, j):
+            check = dot_dic.get((k, k + 1))
+            if check == None:
+                dot_dic[(k, k + 1)] = 1
+            else:
+                dot_dic[(k, k + 1)] += 1
+    
+    values = list(dot_dic.values())
+    answer = [x for x in values if x != 1]
+    
+    return len(answer)
