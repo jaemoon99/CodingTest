2b16e7a847266345b736cd3f39a7ed2cbad4ba2b
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-11-18 17:09:10 +0900

    [level unrated] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,10 +1,6 @@
 def check(lst):
-    c = []
-    for i in range(len(lst) - 1):
-        c.append(lst[i + 1] - lst[i])
-    
-    if len(set(c)) == 1:
-        return lst[-1] + c[0]
+    if lst[1] - lst[0] == lst[2] - lst[1]:
+        return lst[-1] + lst[1] - lst[0]
     else:
         return lst[-1] * (lst[1] // lst[0]) 
     
