85f26b49e694d5aab86444c28a172cd563d88df9
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-12-17 17:07:36 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.py
@@ -0,0 +1,7 @@
+def solution(num_list):
+    # num_list_len = len(num_list)
+    # answer = [0]*num_list_len
+    # for x in num_list:
+    #     answer[num_list_len-1] = x
+    #     num_list_len -= 1  
+    return num_list[::-1]
