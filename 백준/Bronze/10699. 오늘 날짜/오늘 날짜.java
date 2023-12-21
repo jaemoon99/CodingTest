612bad464758ea0bf28346d6113fe73f80bbad79
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-12-21 13:12:35 +0900

    [Bronze V] Title: 오늘 날짜, Time: 192 ms, Memory: 17244 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+
+        System.out.println(localDate);
+    }
+}
