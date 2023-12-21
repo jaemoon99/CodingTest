5aaa0da4969fcf9e017af30cc56d03ef24ba67fd
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2023-12-21 10:03:10 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        double result = (double) a / b;
+
+        System.out.println(result);
+
+    }
+}
