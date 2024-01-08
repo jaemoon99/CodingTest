bf7eab60b5eaead1b28c5dba297f9a5b04d5145d
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-01-08 20:18:42 +0900

    [Bronze IV] Title: 샤틀버스, Time: 208 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if (x > y) {
+            System.out.println(x + y);
+        } else {
+            System.out.println(y - x);
+        }
+
+        scanner.close();
+    }
+}
