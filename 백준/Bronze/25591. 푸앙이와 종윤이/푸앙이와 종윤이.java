bb730eb9415bb9dedbf54e6b2f2cf0810ad9d78c
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-01-09 23:06:02 +0900

    [Bronze IV] Title: 푸앙이와 종윤이, Time: 288 ms, Memory: 19392 KB -BaekjoonHub

---
+++ 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        int a = 100 - x;
+        int b = 100 - y;
+
+        int c = 100 - (a + b);
+        int d = a * b;
+
+        int q = d / 100;
+        int r = d % 100;
+
+        System.out.println(a + " " + b + " " + c + " " + d + " " + q + " " + r);
+        System.out.println((x * y / 100) + " " + (x * y % 100));
+
+        scanner.close();
+    }
+}
