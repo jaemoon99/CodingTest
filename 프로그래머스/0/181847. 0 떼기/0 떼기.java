581a9fdecbf4a9d8b799c20e5760e3684852423e
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-05-20 17:24:26 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,5 +1,11 @@
 class Solution {
     public String solution(String n_str) {
-        return String.valueOf(Integer.parseInt(n_str));
+        // return String.valueOf(Integer.parseInt(n_str));
+        for(int i = 0; i < n_str.length(); i++) {
+            if (n_str.charAt(i) != '0') {
+                return n_str.substring(i);
+            }
+        }
+        return "";
     }
 }
