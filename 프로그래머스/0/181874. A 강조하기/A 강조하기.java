46ac19f88296b37464b4e64914f676d3f23ceb1c
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-05-23 16:49:12 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -2,15 +2,7 @@ class Solution {
     public String solution(String myString) {
         String answer = "";
         
-        for (int i = 0; i < myString.length(); i++) {
-            if (myString.charAt(i) == 'a') {
-                answer += "A";
-            } else if (97 >= myString.charAt(i) && myString.charAt(i) >= 66) {
-                answer += "" + (char) (myString.charAt(i) + 32);
-            } else {
-                answer += "" + myString.charAt(i);
-            }
-        }
+        answer = myString.toLowerCase().replace("a", "A");
         
         return answer;
     }
