519c270c125a35ad46d1959d2a9cfcb9c0876ce9
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-07-01 17:00:51 +0900

    [D2] Title: 숫자 배열 회전, Time: 150 ms, Memory: 58,508 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,29 +1,58 @@
-T = int(input())
-for test_case in range(1, T + 1):
-    N = int(input())
-    matrix = []
-    for _ in range(N):
-        input_row = list(map(str, input().split()))
-        matrix.append(input_row)
-    
-    matrix_90 = []
-    matrix_180 = []
-    matrix_270 = []
-    
-    for x in range(N):
-        append_data = ''
-        for y in range(N):
-            append_data += matrix[y][x]
-        matrix_90.append(append_data[::-1])
-        matrix_270.append(append_data)
-    
-    for x in matrix[::-1]:
-        matrix_180.append(''.join(x[::-1]))
-        
-    matrix_270 = matrix_270[::-1]
-    
-    print(f'#{test_case}')
-    for x in range(N):
-        print(f'{matrix_90[x]} {matrix_180[x]} {matrix_270[x]}')
-            
-            
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int N = sc.nextInt();
+
+            int[][] matrix = new int[N][N];
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+
+            int[][] matrix90 = rotateMatrix(matrix, N);
+            int[][] matrix180 = rotateMatrix(matrix90, N);
+            int[][] matrix270 = rotateMatrix(matrix180, N);
+
+            System.out.println("#" + test_case);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    System.out.print(matrix90[i][j]);
+                }
+                System.out.print(" ");
+
+                for (int j = 0; j < N; j++) {
+                    System.out.print(matrix180[i][j]);
+                }
+                System.out.print(" ");
+
+                for (int j = 0; j < N; j++) {
+                    System.out.print(matrix270[i][j]);
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static int[][] rotateMatrix(int[][] matrix, int N) {
+        int[][] newMatrix = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            int num = N;
+            for (int j = 0; j < N; j++) {
+                newMatrix[i][j] = matrix[--num][i];
+            }
+        }
+        return newMatrix;
+	}
+}
