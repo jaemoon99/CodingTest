7971249572b92ac3d54974049866cc4d025ed6fc
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-07-17 21:01:23 +0900

    [Silver III] Title: 바이러스, Time: 228 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,37 +1,56 @@
 import java.util.*;
+
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        
+
+        /**
+         * count : 감염된 컴퓨터 수
+         * computer : 컴퓨터 수
+         * connect : 직접 연결된 컴퓨터 쌍의 수
+         * checked : 연결된 컴퓨터를 저장하는 배열
+         * visited : 감염된 컴퓨터를 저장하는 배열
+         */
         int count = 0;
         int computer = sc.nextInt();
         int connect = sc.nextInt();
         int[][] checked = new int[computer + 1][computer + 1];
-        int[] visited = new int[computer + 1];  // 방문 여부를 저장하는 배열
-        
-        // 연결된 좌표 저장
+        int[] visited = new int[computer + 1];
+
+        // 연결된 컴퓨터 저장
         for (int i = 0; i < connect; i++) {
             int front = sc.nextInt();
             int rear = sc.nextInt();
             checked[front][rear] = 1;
-            checked[rear][front] = 1; // 양방향 연결 설정
+            checked[rear][front] = 1;
         }
-        
+
         Deque<Integer> queue = new ArrayDeque<>();
+        // 시작
         queue.add(1);
-        visited[1] = 1; // 1번 컴퓨터 방문 처리
+        // 1번 컴퓨터 감염표시
+        visited[1] = 1;
         
+        // 빈 큐가 될 때까지 반복
         while (!queue.isEmpty()) {
+            // 컴퓨터를 큐에서 꺼냄
             int start = queue.pop();
+
+            
+
             for (int end = 1; end <= computer; end++) {
-                if (checked[start][end] == 1 && visited[end] == 0) { // 연결되어 있고 방문하지 않은 경우
+
+                // 현재 컴퓨터와 연결되어 있으면서 감염되지 않은 경우
+                if(checked[start][end] == 1 && visited[end] == 0) {
+                    // 감염표시
+                    visited[end] = 1;
                     queue.add(end);
-                    visited[end] = 1; // 방문 처리
                     count++;
                 }
             }
         }
-        
         System.out.println(count);
     }
 }
