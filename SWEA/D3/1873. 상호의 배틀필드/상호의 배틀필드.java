71cc2b8793ffae700a6c1408482848578c5ae3b7
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-08-23 23:41:00 +0900

    [D3] Title: 상호의 배틀필드, Time: 259 ms, Memory: 42,372 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -6,19 +6,27 @@ public class Solution {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		// 테스트케이스 수
 		int T = sc.nextInt();
 		for (int tc = 1; tc < T + 1; tc++) {
+			// 지도 세로
 			int H = sc.nextInt();
+			// 지도 가로
 			int W = sc.nextInt();
 			
+			// 탱크 좌표
 			int x = 0, y = 0;
+			// 방향 좌표
 			int dx = 0, dy = 0;
+			
 			String[][] map = new String[H][W];
+			// 지도 입력
 			for (int i = 0; i < H; i++) {
 				String input = sc.next();
 				for (int j = 0; j < W; j++) {
 					map[i][j] = input.charAt(j) + "";
 					
+					// 탱크 좌표 확인 상, 하, 좌, 우 각각 경우의 수에 따라 탱크, 방향 좌표 설정
 					if (map[i][j].equals("^")) {
 						x = i;
 						y = j;
@@ -49,9 +57,11 @@ public class Solution {
 				}
 			}
 			
+			// 입력 개수
 			int N = sc.nextInt();
 			String order = sc.next();
 			for (int i = 0; i < N; i++) {
+				// U, D, L, R 각각에 따라 방향좌표를 설정 후 탱크좌표와 계산한 후 계산한 좌표가 지도 내에 있고 이동가능한 경우 위치 갱신
 				if (order.charAt(i) == 'U') {
 					dx = -1;
 					dy = 0;
@@ -104,24 +114,30 @@ public class Solution {
 					map[x][y] = ">";
 				}
 				
+				// S의 경우 현재 방향에 따라 지도 끝 또는 벽을 만날 때까지 순회
 				if (order.charAt(i) == 'S') {
 					int nx = x + dx;
 					int ny = y + dy;
 					while (true) {
+						// 지도 밖이거나 강철 벽인 경우
 						if (nx < 0 || nx >= H || ny < 0 || ny >= W || map[nx][ny].equals("#")) {
 							break;
 						}
 						
+						// 벽돌 벽인 경우 파괴 후 종료
 						if (map[nx][ny].equals("*")) {
 							map[nx][ny] = ".";
 							break;
 						}
 						
+						// 다음 좌표값 설정
 						nx = nx + dx;
 						ny = ny + dy;
 					}
 				}
 			}
+			
+			// 정답 출력
 			System.out.print("#" + tc + " ");
 			for (int i = 0; i < H; i++) {
 				for (int j = 0; j < W; j++) {
