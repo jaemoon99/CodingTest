cbe7a5d762cb13a1abe298e6b06b47c800b55e10
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-08-23 11:43:43 +0900

    [Silver V] Title: 색종이, Time: 176 ms, Memory: 17832 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,36 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        sc.nextLine();
-        int[][] arr = new int[100][100];
-
-        for (int i = 0; i < n; i++) {
-            String str = sc.nextLine();
-            int x = Integer.parseInt(str.split(" ")[0]);
-            int y = Integer.parseInt(str.split(" ")[1]);
-            
-            for (int j = y; j < y + 10; j++){
-                for (int k = x; k < x + 10; k++){
-                    if (arr[j][k] == 0){
-                        arr[j][k] = 1;
-                    }
-                }
-            }
-        }
-
-        int count = 0;
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (arr[i][j] == 1) {
-                    count++;
-                }
-            }
-        }
-        System.out.println(count);
-    }
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// 도형을 그릴 빈 지도
+		int[][] map = new int[100][100];
+		
+		// 결과값
+		int result = 0;
+		// 좌표 개수
+		int N = sc.nextInt();
+		// 좌표 입력
+		for (int i = 0; i < N; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			// 좌표 만큼 도형 그리기
+			for (int j = x; j < x + 10; j++) {
+				for (int k = y; k < y +10; k++) {
+					// 안그린 부분이면
+					if (map[j][k] == 0) {
+						// 그림 표시
+						map[j][k] = 1;
+						// 결과값 증가
+						result++;
+					}
+				}
+			}
+		}
+		// 정답 출력
+		System.out.println(result);
+	}
 }
