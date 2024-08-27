864b573fdff143425166c36a2b9f2be0af728a1a
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-08-27 15:30:43 +0900

    [Silver II] Title: 섬의 개수, Time: 352 ms, Memory: 35736 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -4,10 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 
+	/**
+	 * w : 지도 너비
+	 * h : 지도 높이
+	 * map : 지도
+	 * dx, dy : 8방 탐색용 배열
+	 */
     static int w;
     static int h;
     static int map[][];
-    static int visited[][];
     static int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
     static int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
 
@@ -18,12 +23,13 @@ public class Main {
             w = sc.nextInt();
             h = sc.nextInt();
 
+            // 높이, 너비가 둘 다 0이면 종료
             if (w == 0 && h == 0) {
                 break;
             }
 
+            // 섬 입력
             map = new int[h][w];
-            visited = new int[h][w];
             for (int i = 0; i < h; i++) {
                 for (int j = 0; j < w; j++) {
                     map[i][j] = sc.nextInt();
@@ -33,29 +39,35 @@ public class Main {
             int result = 0;
             for (int i = 0; i < h; i++) {
                 for (int j = 0; j < w; j++) {
-                    if (map[i][j] == 1 && visited[i][j] == 0) {
-                        //bfs(i, j);  // 사용하고자 하는 메서드를 선택합니다.
-                        dfs(i, j); // bfs 대신 dfs를 사용하려면 이 줄의 주석을 해제하세요.
+                	// 섬인경우 함수 호출
+                    if (map[i][j] == 1) {
+                    	dfs(i, j);
+//                        bfs(i, j);
                         result++;
                     }
                 }
             }
+            
+            // 결과 출력
             System.out.println(result);
         }
     }
 
     static void dfs(int x, int y) {
+    	// 방문 표시
     	map[x][y] = 0;
-//        visited[x][y] = 1;  // 방문 체크 추가
 
+    	// 8방 순회
         for (int i = 0; i < 8; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx < 0 || nx >= h || ny < 0 || ny >= w || visited[nx][ny] == 1 || map[nx][ny] == 0) {
+            // 지도 밖이거나 섬이 아닌 경우 무시
+            if (nx < 0 || nx >= h || ny < 0 || ny >= w || map[nx][ny] == 0) {
                 continue;
             }
 
+            // 다음 섬 호출
             dfs(nx, ny);
         }
     }
@@ -63,22 +75,26 @@ public class Main {
     static void bfs(int x, int y) {
         Deque<int[]> queue = new ArrayDeque<>();
         queue.offer(new int[]{x, y});
+        // 방문 표시
         map[x][y] = 0;
-//        visited[x][y] = 1;  // **수정된 부분**: 방문 체크 추가
 
+        // 큐가 빌 때까지 반복
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
 
+            // 8방 순회
             for (int i = 0; i < 8; i++) {
                 int nx = poll[0] + dx[i];
                 int ny = poll[1] + dy[i];
 
-                if (nx < 0 || nx >= h || ny < 0 || ny >= w || visited[nx][ny] == 1 || map[nx][ny] == 0) {
+                // 지도 밖이거나 섬이 아닌 경우 무시
+                if (nx < 0 || nx >= h || ny < 0 || ny >= w || map[nx][ny] == 0) {
                     continue;
                 }
 
+                // 방문 표시
                 map[nx][ny] = 0;
-//                visited[nx][ny] = 1;  // **수정된 부분**: 방문 체크 추가
+                // 인접 섬 큐에 삽입
                 queue.offer(new int[]{nx, ny});
             }
         }
