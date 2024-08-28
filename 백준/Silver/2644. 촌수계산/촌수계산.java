f29ad59a6b1425b106e9837a0fe210549a9ea610
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-08-28 15:33:04 +0900

    [Silver II] Title: 촌수계산, Time: 172 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -2,57 +2,72 @@ import java.util.*;
 
 public class Main {
 
-    static ArrayList<Integer>[] graph;
-    static boolean[] visited;
+	/**
+	 * map : 사람 배열
+	 * v : 방문 체크용 배열
+	 */
+    static ArrayList<Integer>[] map;
+    static boolean[] v;
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 사람 수
         int n = sc.nextInt();
         
-        graph = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
+        map = new ArrayList[n + 1];
+        v = new boolean[n + 1];
         
         for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+            map[i] = new ArrayList<>();
         }
 
         int p1 = sc.nextInt();
         int p2 = sc.nextInt();
         
+        // 촌 연결
         int m = sc.nextInt();
         for (int i = 0; i < m; i++) {
             int num1 = sc.nextInt();
             int num2 = sc.nextInt();
-            graph[num1].add(num2);
-            graph[num2].add(num1);
+            map[num1].add(num2);
+            map[num2].add(num1);
         }
 
+        // 탐색
         int result = bfs(p1, p2);
+        // 결과 출력
         System.out.println(result);
     }
     
     static int bfs(int start, int target) {
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{start, 0});
-        visited[start] = true;
+        // 방문 표시
+        v[start] = true;
         
+        // 큐가 빌 때까지 탐색
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int currentNode = current[0];
-            int currentDepth = current[1];
+            int[] poll = queue.poll();
+            int x = poll[0];
+            int y = poll[1];
             
-            if (currentNode == target) {
-                return currentDepth;
+            // 타겟과 통일한 경우
+            if (x == target) {
+                return y;
             }
             
-            for (int neighbor : graph[currentNode]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.add(new int[]{neighbor, currentDepth + 1});
+            // 본인과 연결된 사람 순회
+            for (int p : map[x]) {
+            	// 방문하지 않은 경우
+                if (!v[p]) {
+                	// 방문 표시
+                    v[p] = true;
+                    // 다음 사람 삽입
+                    queue.add(new int[]{p, y + 1});
                 }
             }
         }
         
-        return -1;  // 연결되지 않은 경우
+        return -1;
     }
 }
