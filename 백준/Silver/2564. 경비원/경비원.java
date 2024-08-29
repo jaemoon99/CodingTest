641abd63b2824914b1317c323362f626109637d7
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-08-29 14:02:01 +0900

    [Silver I] Title: 경비원, Time: 176 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Silver/2564. 경비원/경비원.java
+++ 백준/Silver/2564. 경비원/경비원.java
@@ -1,46 +1,45 @@
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
 
         // 블록의 가로 길이(w)와 세로 길이(h) 입력
-        int w = scanner.nextInt();
-        int h = scanner.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
 
         // 상점의 수(n) 입력
-        int n = scanner.nextInt();
+        int n = sc.nextInt();
         int[] stores = new int[n];
 
         // 상점들의 위치를 입력 받아서 배열에 저장
         for (int i = 0; i < n; i++) {
-            int direction = scanner.nextInt();
-            int distance = scanner.nextInt();
-            stores[i] = getPosition(w, h, direction, distance);
+            int dir = sc.nextInt();
+            int dis = sc.nextInt();
+            stores[i] = getPosition(w, h, dir, dis);
         }
 
+
         // 경비원의 위치 입력
-        int guardDirection = scanner.nextInt();
-        int guardDistance = scanner.nextInt();
-        int guardPosition = getPosition(w, h, guardDirection, guardDistance);
+        int gDir = sc.nextInt();
+        int gDis = sc.nextInt();
+        int gPosition = getPosition(w, h, gDir, gDis);
 
         // 총 둘레 길이 계산
-        int totalLength = 2 * (w + h);
+        int tLen = 2 * (w + h);
 
         // 최단 거리 계산
-        int totalDistance = 0;
+        int tDis = 0;
         for (int store : stores) {
-            int clockwiseDist = Math.abs(store - guardPosition);
-            int counterClockwiseDist = totalLength - clockwiseDist;
-            totalDistance += Math.min(clockwiseDist, counterClockwiseDist);
+            int dis1 = Math.abs(store - gPosition);
+            int dis2 = tLen - dis1;
+            tDis += Math.min(dis1, dis2);
         }
 
         // 결과 출력
-        System.out.println(totalDistance);
-
-        scanner.close();
+        System.out.println(tDis);
     }
 
     // 위치를 둘레를 따라 일렬로 펼쳐진 직선상의 좌표로 변환하는 함수
