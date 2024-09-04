cff37f318df25fc28df74fc2a442d087a36bb633
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-09-04 23:35:17 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 163 ms, Memory: 26,984 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class Solution {
-    static int N, K, maxLength;
+    static int N, K, result;
     static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = { -1, 1, 0, 0 }; // 상, 하, 좌, 우
-    static int[] dy = { 0, 0, -1, 1 }; // 상, 하, 좌, 우
-
+    static boolean[][] v;
+    // 4방탐색
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt(); // 테스트 케이스 수
@@ -15,17 +16,17 @@ public class Solution {
             N = sc.nextInt(); // 지도의 크기
             K = sc.nextInt(); // 최대 깎을 수 있는 깊이
             map = new int[N][N];
-            visited = new boolean[N][N];
-            maxLength = 0;
+            v = new boolean[N][N];
+            result = 0;
 
-            int highest = 0; // 가장 높은 지형의 높이
+            int h = 0; // 가장 높은 지형의 높이
 
             // 지도 데이터 입력 및 가장 높은 지형 찾기
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     map[i][j] = sc.nextInt();
-                    if (map[i][j] > highest) {
-                        highest = map[i][j];
+                    if (map[i][j] > h) {
+                        h = map[i][j];
                     }
                 }
             }
@@ -33,43 +34,41 @@ public class Solution {
             // 가장 높은 지형에서 등산로 시작
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (map[i][j] == highest) {
+                    if (map[i][j] == h) {
                         dfs(i, j, 1, false); // (i, j)에서 길이 1, 깎지 않음으로 DFS 시작
                     }
                 }
             }
 
-            System.out.println("#" + t + " " + maxLength);
+            System.out.println("#" + t + " " + result);
         }
-        sc.close();
     }
 
-    // DFS 탐색
-    private static void dfs(int x, int y, int length, boolean cut) {
+    private static void dfs(int x, int y, int len, boolean cut) {
         // 최대 길이 갱신
-        if (length > maxLength) {
-            maxLength = length;
+        if (len > result) {
+            result = len;
         }
 
-        visited[x][y] = true; // 현재 위치 방문 처리
+        v[x][y] = true; // 현재 위치 방문 처리
 
         // 4방향 탐색
         for (int d = 0; d < 4; d++) {
             int nx = x + dx[d];
             int ny = y + dy[d];
 
-            if (nx >= 0 && ny >= 0 && nx < N && ny < N && !visited[nx][ny]) { // 지도 안에 있고 방문하지 않은 경우
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N && !v[nx][ny]) { // 지도 안에 있고 방문하지 않은 경우
                 if (map[nx][ny] < map[x][y]) { // 다음 위치가 현재보다 낮은 경우
-                    dfs(nx, ny, length + 1, cut);
+                    dfs(nx, ny, len + 1, cut);
                 } else if (!cut && map[nx][ny] - K < map[x][y]) { // 깎지 않았고, 깎아서 이동할 수 있는 경우
                     int originalHeight = map[nx][ny];
                     map[nx][ny] = map[x][y] - 1; // 지형 깎기
-                    dfs(nx, ny, length + 1, true); // 깎았음을 표시하고 DFS 진행
+                    dfs(nx, ny, len + 1, true); // 깎았음을 표시하고 DFS 진행
                     map[nx][ny] = originalHeight; // 원상 복구
                 }
             }
         }
 
-        visited[x][y] = false; // 백트래킹을 위해 방문 표시 제거
+        v[x][y] = false; // 백트래킹을 위해 방문 표시 제거
     }
 }
