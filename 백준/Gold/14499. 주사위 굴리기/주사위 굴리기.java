ed8614655fe947f73c5f40c31a8b6038f9b487c3
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-10-09 20:58:43 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 188 ms, Memory: 18332 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -1,33 +1,79 @@
 import java.util.Scanner;
 
 public class Main {
-    static int[][] directions = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}}; // 동, 서, 북, 남
-    static int[] dice = new int[6]; // 주사위의 상태를 나타내는 배열
+	
+    static int[] dx = new int[] {0, 0, -1, 1};
+    static int[] dy = new int[] {1, -1, 0, 0};
+    static int[] dice = new int[6];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int sx = sc.nextInt();
+        int sy = sc.nextInt();
+        int commandCnt = sc.nextInt();
+
+        int[][] board = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] commands = new int[commandCnt];
+        for (int i = 0; i < commandCnt; i++) {
+            commands[i] = sc.nextInt();
+        }
+
+        for (int command : commands) {
+            int nx = sx + dx[command - 1];
+            int ny = sy + dy[command - 1];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                continue;
+            }
+
+            sx = nx;
+            sy = ny;
+
+            moveDice(command - 1);
+
+            if (board[sx][sy] == 0) {
+                board[sx][sy] = dice[5];
+            } else {
+                dice[5] = board[sx][sy];
+                board[sx][sy] = 0;
+            }
+
+            System.out.println(dice[0]);
+        }
+    }
+    
     static void moveDice(int direction) {
-        int[] temp = dice.clone(); // 주사위의 현재 상태를 복사
-        if (direction == 0) { // 동
+        int[] temp = dice.clone();
+        if (direction == 0) {
             dice[0] = temp[3];
             dice[2] = temp[0];
             dice[3] = temp[5];
             dice[5] = temp[2];
             dice[1] = temp[1];
             dice[4] = temp[4];
-        } else if (direction == 1) { // 서
+        } else if (direction == 1) {
             dice[0] = temp[2];
             dice[2] = temp[5];
             dice[3] = temp[0];
             dice[5] = temp[3];
             dice[1] = temp[1];
             dice[4] = temp[4];
-        } else if (direction == 2) { // 북
+        } else if (direction == 2) {
             dice[0] = temp[1];
             dice[1] = temp[5];
             dice[4] = temp[0];
             dice[5] = temp[4];
             dice[2] = temp[2];
             dice[3] = temp[3];
-        } else if (direction == 3) { // 남
+        } else if (direction == 3) {
             dice[0] = temp[4];
             dice[1] = temp[0];
             dice[4] = temp[5];
@@ -35,55 +81,5 @@ public class Main {
             dice[2] = temp[2];
             dice[3] = temp[3];
         }
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        int k = scanner.nextInt();
-
-        int[][] board = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                board[i][j] = scanner.nextInt();
-            }
-        }
-
-        int[] commands = new int[k];
-        for (int i = 0; i < k; i++) {
-            commands[i] = scanner.nextInt();
-        }
-
-        for (int command : commands) {
-            int dx = directions[command - 1][0];
-            int dy = directions[command - 1][1];
-
-            int nx = x + dx;
-            int ny = y + dy;
-
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                continue; // 범위를 벗어나면 무시
-            }
-
-            x = nx;
-            y = ny;
-
-            moveDice(command - 1);
-
-            if (board[x][y] == 0) {
-                board[x][y] = dice[5];
-            } else {
-                dice[5] = board[x][y];
-                board[x][y] = 0;
-            }
-
-            System.out.println(dice[0]);
-        }
-
-        scanner.close();
     }
 }
