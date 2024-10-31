b606e9715b0a87f47bf413889e79548dcf3993f1
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-10-31 17:50:27 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2168 ms, Memory: 253640 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-
-	static int n, m, map[], v[];
 	
 	public static void main(String[] args) {
 	
@@ -11,18 +9,17 @@ public class Main {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		
-		map = new int[n + 1];
-		int[] sum = new int[n + 1];
+		int[] map = new int[n + 1];
 		for (int i = 1; i <= n; i++) {
-			map[i] = sc.nextInt();
-			sum[i] = map[i] + sum[i - 1];
+			int num = sc.nextInt();
+			map[i] = num + map[i - 1];
 		}
 		
 		
 		for (int i = 0; i < m; i++) {
 			int j = sc.nextInt();
 			int k = sc.nextInt();
-			System.out.println(sum[k] - sum[j - 1]);
+			System.out.println(map[k] - map[j - 1]);
 		}
 	}	
 }
