f34235f40c7a8f7d92da87c769a7c3eb938a3b2e
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-12-11 17:16:36 +0900

    [Silver II] Title: N과 M (11), Time: 524 ms, Memory: 244188 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	static int n, m, map[], sel[], v[];
+	static StringBuilder sb;
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		map = new int[n];
+		sel = new int[m];
+		
+		for (int i = 0; i < n; i++) {
+			map[i] = sc.nextInt();
+		}
+		
+		v = new int[n];
+		
+		Arrays.sort(map);
+		
+		per(0);
+		
+		System.out.println(sb.toString());
+		
+		sc.close();
+	}
+	
+	static void per(int cnt) {
+		if (cnt == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(sel[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			if (i > 0 && map[i] == map[i - 1] && v[i - 1] == 0) {
+				continue;
+			}
+			
+			sel[cnt] = map[i];
+			per(cnt + 1);
+		}
+	}
+}
