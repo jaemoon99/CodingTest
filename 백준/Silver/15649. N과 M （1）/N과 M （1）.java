fdb409ac1f6541e5123aa08880fe51d9f0307ccc
jaemoon <103016314+jaemoon99@users.noreply.github.com>
2024-12-11 13:13:55 +0900

    [Silver III] Title: N과 M (1), Time: 1496 ms, Memory: 66812 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,50 +1,39 @@
 import java.util.Scanner;
 
-
 public class Main {
-    static int N;
-    static int M;
-    static int[] map;
-    static int[] sel;
-    static int[] v;
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-        M = sc.nextInt();
-
-        map = new int[N];
-        sel = new int[M];
-        v = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            map[i] = i + 1;
-        }
-
-        dfs(0);
-    }
-
-    static void dfs(int idx) {
-        // 종료
-        if (idx == M) {
-            for (int i = 0; i < M; i++) {
-                System.out.print(sel[i] + " ");
-            }
-            System.out.println();
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            if (v[i] == 1) {
-                continue;
-            }
-
-            sel[idx] = map[i];
-            v[i] = 1;
-            dfs(idx + 1);
-            v[i] = 0;
-        }
-    }
+	
+	static int n, m, sel[], v[];
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		sel = new int[m];
+		v = new int[n];
+		
+		com(0);
+		
+		sc.close();
+	}
+	
+	static void com(int cnt) {
+		if (cnt == m) {
+			for (int i = 0; i < m; i++) {
+				System.out.print(sel[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			if (v[i] == 0) {
+				v[i] = 1;
+				sel[cnt] = i + 1;
+				com(cnt + 1);
+				v[i] = 0;
+			}
+		}
+	}
 }
