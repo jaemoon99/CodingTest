8d69ee3bd746345a0cd7d4b8b2570a227f6084d5
Jaemoon <woans5970@naver.com>
2025-09-06 20:52:44 +0900

    [Silver II] Title: 통계학, Time: 936 ms, Memory: 298468 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,63 +1,70 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		int sum = 0;
-		int[] nums = new int[n];
-		
-		Map<Integer, Integer> maps = new HashMap<>();
-		for (int i = 0; i < n; i++) {
-			nums[i] = sc.nextInt();
-			
-			sum += nums[i];
-			
-			if (!maps.containsKey(nums[i])) {
-				maps.put(nums[i], 1);
-			} else {
-				maps.put(nums[i], maps.get(nums[i]) + 1);
-			}
-			
-		}
-		
-		sc.close();
-		
-		Arrays.sort(nums);
-		
-		int check = 0;
-		int cnt = 0;
-		int value = 0;
-		for (int num : nums) {
-			if (value == num && maps.get(num) == cnt) {
-				continue;
-			}
-			
-			if (maps.get(num) > cnt) {
-				cnt = maps.get(num);
-				check = 1;
-				value = num;
-				continue;
-			}
-			
-			if (check == 1 && maps.get(num) == cnt) {
-				check = 0;
-				value = num;
-			}
-		}
-		
-		System.out.println((int) Math.round((double) sum / n));
-		System.out.println(nums[n / 2]);
-		System.out.println(value);
-		System.out.println(nums[n - 1] - nums[0]);
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+
+        int[] freq = new int[8001];  // -4000 ~ 4000 (총 8001개)
+        long sum = 0;
+        int min = 4001, max = -4001;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            sum += x;
+            freq[x + 4000]++;
+            
+            if (x < min) {
+            	min = x;
+            }
+            
+            if (x > max) {
+            	max = x;
+            }
+        }
+
+        sc.close();
+
+        // 평균 (반올림)
+        int mean = (int) Math.round((double) sum / n);
+
+        // 중앙값
+        int target = n / 2 + 1;
+        int acc = 0;
+        int median = 0;
+        for (int v = min; v <= max; v++) {
+            acc += freq[v + 4000];
+            if (acc >= target) {
+                median = v;
+                break;
+            }
+        }
+
+        // 최빈값 (개수가 같다면 두 번째로 작은 값)
+        int maxFreq = 0;
+        for (int v = min; v <= max; v++) {
+            if (freq[v + 4000] > maxFreq) maxFreq = freq[v + 4000];
+        }
+
+        int mode = 0, seen = 0;
+        for (int v = min; v <= max; v++) {
+            if (freq[v + 4000] == maxFreq) {
+                seen++;
+                mode = v;
+                
+                if (seen == 2) {
+                	break; // 두 번째 최빈값이면 멈춤
+                }
+            }
+        }
+
+        // 범위
+        int range = max - min;
+
+        System.out.println(mean);
+        System.out.println(median);
+        System.out.println(mode);
+        System.out.println(range);
+    }
 }
