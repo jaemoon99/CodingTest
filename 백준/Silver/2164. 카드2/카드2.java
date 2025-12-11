2512a0cd9acee20f554b0bbfd1c39869cb867165
Jaemoon <woans5970@naver.com>
2025-12-11 17:25:59 +0900

    [Silver IV] Title: 카드2, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,30 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-		int n = Integer.parseInt(br.readLine());
+        br.close();
+        
+        int power = 1;
+        while (power * 2 <= n) {
+            power *= 2;
+        }
 
-		br.close();
-
-		Deque<Integer> dq = new ArrayDeque<>();
-		for (int i = 0; i < n; i++) {
-			dq.add(i + 1);
-		}
-
-		while (dq.size() != 1) {
-			dq.poll();
-			dq.offer(dq.poll());
-		}
-
-		System.out.println(dq.poll());
-
-	}
+        if (n == power) {
+            System.out.println(n);
+        } else {
+            System.out.println(2 * (n - power));
+        }
+    }
 }
