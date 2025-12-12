925037746bd80178a0c66540183c24bc886053cc
Jaemoon <woans5970@naver.com>
2025-12-12 10:15:27 +0900

    [Silver IV] Title: 괄호, Time: 88 ms, Memory: 12628 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		Stack<String> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String[] strs = st.nextToken().split("");
+
+			for (String str : strs) {
+				if (stack.isEmpty()) {
+					stack.push(str);
+					continue;
+				}
+
+				if (stack.peek().equals("(") && str.equals(")")) {
+					stack.pop();
+					continue;
+				}
+
+				stack.push(str);
+			}
+
+			if (stack.isEmpty()) {
+				sb.append("YES").append('\n');
+			} else {
+				sb.append("NO").append('\n');
+			}
+
+			stack.clear();
+		}
+
+		br.close();
+
+		System.out.println(sb);
+	}
+}
