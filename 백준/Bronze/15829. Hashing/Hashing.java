da202f8d342acd329412440082a0aa7e4b260158
Jaemoon <woans5970@naver.com>
2025-12-17 14:28:56 +0900

    [Bronze II] Title: Hashing, Time: 68 ms, Memory: 11528 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,23 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int l = sc.nextInt();
-		String str = sc.next();
-		
-		sc.close();
-		
-		int result = 0;
-		for (int i = 0; i < l; i++) {
-			int strToNum = str.charAt(i) - 96;
-			result += strToNum * (int) Math.pow(31, i);
-		}
-		
-		System.out.println(result);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int l = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        
+        br.close();
 
+        final long MOD = 1234567891L;
+
+        long result = 0;
+        long pow = 1;
+
+        for (int i = 0; i < l; i++) {
+            long val = (str.charAt(i) - 'a' + 1);
+            result = (result + val * pow) % MOD;
+            pow = (pow * 31) % MOD;
+        }
+
+        System.out.println(result);
+    }
 }
