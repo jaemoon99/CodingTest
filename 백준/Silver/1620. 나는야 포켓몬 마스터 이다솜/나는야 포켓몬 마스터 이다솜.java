4e5a89370e68def798fa0f0d4ed5d189717ab183
Jaemoon <woans5970@naver.com>
2025-12-18 11:24:01 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 412 ms, Memory: 54812 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] line = br.readLine().split(" ");
 
@@ -37,6 +40,8 @@ public class Main {
 
         br.close();
 
-        System.out.println(sb);
+        bw.write(sb.toString());
+        
+        bw.flush();
     }
 }
