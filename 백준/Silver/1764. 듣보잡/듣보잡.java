6b9661135094fbb3e5f6f2976a40034d5749736f
Jaemoon <woans5970@naver.com>
2025-12-18 15:53:18 +0900

    [Silver IV] Title: 듣보잡, Time: 220 ms, Memory: 25868 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -13,34 +14,29 @@ public class Main {
         int n = Integer.parseInt(line[0]);
         int m = Integer.parseInt(line[1]);
 
-        Map<String, Integer> map = new TreeMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
             String name = br.readLine();
             map.put(name, 1);
         }
 
+        Map<String, Integer> sortMap = new TreeMap<>();
         for (int i = 0; i < m; i++) {
             String name = br.readLine();
-            if (!map.containsKey(name)) {
-                map.put(name, 1);
-                continue;
+            if (map.containsKey(name)) {
+                sortMap.put(name, 1);
             }
-
-            map.put(name, map.get(name) + 1);
         }
 
         br.close();
 
-        int cnt = 0;
         StringBuilder sb = new StringBuilder();
-        for (String name : map.keySet()) {
-            if (map.get(name) == 2) {
-                sb.append(name).append("\n");
-                cnt++;
-            }
+        sb.append(sortMap.size()).append("\n");
+        for (String name : sortMap.keySet()) {
+            sb.append(name).append("\n");
         }
 
-        System.out.println(sb.insert(0, "\n").insert(0, cnt));
+        System.out.println(sb);
 
     }
 }
