741f4abfbe780a95d033a1bf387bc9958a5d0f48
Jaemoon <woans5970@naver.com>
2026-02-26 15:19:11 +0900

    [level 2] Title: [3차] 방금그곡, Time: 16.78 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String solution(String m, String[] musicinfos) {
+        m = m.replace("C#", "ㅊ").replace("D#", "ㅇ").replace("F#", "ㄹ")
+             .replace("G#", "ㅎ").replace("A#", "ㅁ").replace("E#", "ㄷ").replace("B#", "ㅠ");
+
+        String answer = "(None)";
+        int bestTime = -1;
+
+        for (String musicinfo : musicinfos) {
+            String[] split = musicinfo.split(",");
+
+            String[] s = split[0].split(":");
+            int start = Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
+
+            String[] e = split[1].split(":");
+            int end = Integer.parseInt(e[0]) * 60 + Integer.parseInt(e[1]);
+
+            int time = end - start;
+            String title = split[2];
+
+            String score = split[3].replace("C#", "ㅊ").replace("D#", "ㅇ").replace("F#", "ㄹ")
+                                   .replace("G#", "ㅎ").replace("A#", "ㅁ").replace("E#", "ㄷ").replace("B#", "ㅠ");
+
+            StringBuilder played = new StringBuilder(time);
+            int len = score.length();
+            for (int i = 0; i < time; i++) {
+                played.append(score.charAt(i % len));
+            }
+
+            if (played.indexOf(m) != -1) {
+                if (time > bestTime) {
+                    bestTime = time;
+                    answer = title;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
