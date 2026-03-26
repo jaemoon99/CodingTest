7c95cd1e10a9c2d5e423e42478379d0423215f27
Jaemoon <woans5970@naver.com>
2026-03-26 17:06:22 +0900

    [level 2] Title: H-Index, Time: 0.94 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+        
+        for (int i = citations.length - 1; i >= 0; i--) {
+            
+            if (citations[i] > answer) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
